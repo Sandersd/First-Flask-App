115a378dbf4821cde403af015bc2f3a0c02d7d10
Dylan Sanders <sandersd27@gmail.com>
2016-04-20 02:30:38 +0000

    add db

--- flask_tut2.py
+++ flask_tut2.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="DylanMSanders",
+    password="npntpvld4",
+    hostname="DylanMSanders.mysql.pythonanywhere-services.com",
+    databasename="DylanMSanders$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all())
+
+    comment = Comment(content = request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+
+    return redirect(url_for("index"))
 
